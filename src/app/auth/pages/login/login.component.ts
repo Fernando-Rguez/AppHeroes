e520e0afbe94d2e43c941b7fe7a4fbd763e97935
Fernando Rodriguez <fernando.rodriguez35@my.unitec.edu.mx>
2022-06-06 21:04:45 -0500

    primer commit

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router,
+              private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    //Ir al backend
+    //Un usuario
+
+    this.authService.login()
+    .subscribe( resp =>{
+      console.log(resp);
+
+
+      if (resp.id){
+        this.router.navigate(['./heroes']);
+      }
+    })
+  }
+
+  ingeresarSinLogin() {
+    this.authService.logout(); 
+    this.router.navigate(['/heroes'])
+  }
+
+}
